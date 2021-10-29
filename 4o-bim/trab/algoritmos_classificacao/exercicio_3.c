7e7b6118a7d1f74298e2e7b53ec0aab23159bb00
Nathanael <dev@nathabonfim59.com>
2021-10-28 23:31:09 -0300

    Exercício 3

--- 4o-bim/trab/algoritmos_classificacao/exercicio_3.c
+++ 4o-bim/trab/algoritmos_classificacao/exercicio_3.c
@@ -1,37 +1,34 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #define QUANTIDADE_POSICOES 5
 
-int main(void) {
-    int valores[QUANTIDADE_POSICOES];
+int main() {
+    int v[QUANTIDADE_POSICOES], n = QUANTIDADE_POSICOES, aux, i, c, teste = 1;
     int passos = 0;
-    int ordenado = 1;
 
     for (int i = 0; i < QUANTIDADE_POSICOES; i++) {
         printf("Digite um valor para a posição %d: ", i);
-        scanf("%d", &valores[i]);
+        scanf("%d", &v[i]);
     }
 
 
-    int aux;
-    ordenado = 1;
+    for (i = 0; (i < n - 1) && (teste == 1); i++) {
+        teste = 0;
 
-    for (int i = 0; i < QUANTIDADE_POSICOES - 1; i++) {
-        for (int c = 0; (c < (QUANTIDADE_POSICOES - i - 1) && ordenado == 1); c++) {
-            if (valores[c] > valores[c + 1]) {
-                ordenado = 1;
-                aux = valores[c];
-                valores[c] = valores[c + 1];
-                valores[c + 1] = aux;
-            }
+        for (c = 0; c < n - i - 1; c++) {
             passos++;
+            if (v[c] > v[c + 1]) {
+                teste = 1;
+                aux = v[c];
+                v[c] = v[c + 1];
+                v[c + 1] = aux;
+            }
         }
     }
 
-
     printf("--------------\n");
     for (int i = 0; i < QUANTIDADE_POSICOES; i++) {
-        printf("Posição %d: %d\n", i, valores[i]);
+        printf("Posição %d: %d\n", i, v[i]);
     }
     printf("--------------\n");
     printf("Quantidade de passos necessários: %d\n", passos);

334422b489aa66601bd95df79b8bc59f5572ee70
Nathanael <dev@nathabonfim59.com>
2021-10-28 22:30:50 -0300

    Exercicio 4

---
+++ 4o-bim/trab/algoritmos_classificacao/exercicio_4.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define QUANTIDADE_POSICOES 5
+
+int main(void) {
+    int a[5], n = 5, troca = 1, ut = 0, c, aux, m = n - 1;
+    int passos = 0;
+
+    for (int i = 0; i < 5; i++) {
+        printf("Digite um valor para a posição %d: ", i);
+        scanf("%d", &a[i]);
+    }
+
+    while (troca == 1) {
+        troca = 0;
+
+        for (c = 0; c < m; c++) {
+            passos++;
+            if (a[c] > a[c + 1]) {
+                aux = a[c];
+                a[c] = a[c + 1];
+                a[c + 1] = aux;
+
+                ut = c;
+                troca = 1;
+            }
+        }
+    m = ut;
+    }
+
+    printf("--------------\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Posição %d: %d\n", i, a[i]);
+    }
+    printf("--------------\n");
+    printf("Quantidade de passos necessários: %d\n", passos);
+}
